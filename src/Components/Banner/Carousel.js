41e47a925f76c6a7b091a93e63ddd2558e3cc09d
NoelW95 <53453274+noelw19@users.noreply.github.com>
2020-12-10 16:49:04 +1300

    working

---
+++ src/Components/Banner/Carousel.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactSwipe from 'react-swipe';
+
+import img from '../../Images/Ferment2.jpg'
+import img1 from '../../Images/apple-cider-4676465_1920.jpg'
+import img2 from '../../Images/pexels-angele-j-128403.jpg'
+
+
+const Carousel = () => {
+  let reactSwipeEl;
+
+  return (
+    <div className='swipeContainer'>
+      <ReactSwipe
+        className="carousel"
+        swipeOptions={{ continuous: true, widthOfSiblingSlidePreview: 10 }}
+        ref={el => (reactSwipeEl = el)}
+      >
+         {/* need to sort img sizing as the carousel gets too large when 
+         diff size images are used. */}
+        <div className='imgContainer'><img src={ img } alt='webiste1'/></div>
+        <div className='imgContainer'><img src={ img1 } alt='webiste1'/></div>
+        <div className='imgContainer'><img src={ img2 } alt='webiste1'/></div>
+      </ReactSwipe>
+      <button onClick={() => reactSwipeEl.next()}>Next</button>
+      <button onClick={() => reactSwipeEl.prev()}>Previous</button>
+    </div>
+  );
+};
+
+export default Carousel;
